3495b86dcd17c8f767d440ff080ed6dde3bcd57c
Nux-xader <sumgmng@gmail.com>
2021-12-26 22:33:58 +0700

    learn state widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  int num = 0;
+  void btn_handler() {
+    setState() {
+      num = num+1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +19,18 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.deepPurple,
           title: Text("Nux App"),
-        ), 
-        body: Container(
-          color: Colors.black87,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(num.toString()),
+              RaisedButton(
+                child: Text("Add"),
+                onPressed: btn_handler,
+              )
+            ],
+          ),
         ),
       ),
     );
